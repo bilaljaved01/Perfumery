279eb93e75ff7a6be1036b01161a7e28b6268848
bilaljaved01 <bilaljaved1002@gmail.com>
2025-04-24 14:09:04 +0530

    updated route

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -14,7 +14,7 @@ export async function POST(request) {
 
         //calculate order amount
 
-        const amount = await items.reduce(async(accessedDynamicData,item)=>{
+        const amount = await items.reduce(async(acc,item)=>{
             const product = await Product.findById(item.product);
             return acc + product.offerPrice * item.quantity;
         },0)
